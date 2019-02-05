e98d4a47f1af7a941dfa8f1328d6a5564a2328e0
Boris Bobol <bobol.boris@gmail.com>
2019-02-05 16:49:27 +0300

    Fix API path

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,6 @@ Route::prefix('login')->group(function () {
     Route::match(['get', 'head'], 'back', 'Auth\LoginController@back')->name('back');
 });
 
-//Route::prefix('password')->group(function () {
-//    Route::post('email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-//    Route::match(['get', 'head'], 'reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-//    Route::post('reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-//    Route::match(['get', 'head'], 'reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-//});
-
-//Route::match(['get', 'head'], 'register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-//Route::post('register', 'Auth\RegisterController@register');
-
-
 /*Cabinet*/
 
 Route::match(['get', 'post'], '/', 'ChildrenController@indexAction')->name('index');
@@ -32,17 +21,18 @@ Route::match(['get', 'post'], '/child/{id}', 'ChildrenController@childAction')->
 Route::get('/settings', 'SettingsController@indexAction')->name('settings');
 Route::post('/settings', 'SettingsController@saveAction');
 
+/*Report*/
 Route::post('/report', 'ChildrenController@reportAction')->name('report');
-
 Route::post('/get-access-by-date', 'ChildrenController@getAccessByDateAction');
 
-
+/*Key*/
 Route::prefix('key')->group(function () {
     Route::post('lock', 'ChildrenController@lockKeyAction');
     Route::post('unlock', 'ChildrenController@unlockKeyAction');
 });
 
-/*Report*/
-
+/*API*/
+Route::post('/auth/{slug}', 'FrontController@getFront')->where('slug', '([A-z\d-\/_.]+)?');
 Route::post('/front/{slug}', 'FrontController@getFront')->where('slug', '([A-z\d-\/_.]+)?');
+
 
